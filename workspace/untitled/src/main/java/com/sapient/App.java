720b1d876d8e4fb719ebc3d22364a8ed83223e3f
Naveen <adith.naveen@gmail.com>
2024-03-11 15:28:38 +0530

    update CRUD operations

--- workspace/untitled/src/main/java/com/sapient/App.java
+++ workspace/untitled/src/main/java/com/sapient/App.java
@@ -4,19 +4,31 @@ import com.sapient.beans.Employee;
 import com.sapient.contracts.IEmployeeDAO;
 import com.sapient.dao.EmployeeDAO;
 
+import java.util.List;
 
 
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        Employee employee = new Employee(101, "Naveen", 12345);
+        Employee employee = new Employee(103, "Yogesh", 33221);
 
         IEmployeeDAO dao = new EmployeeDAO();
-        Employee insertedEmployee = dao.insertEmployee(employee);
+//        Employee insertedEmployee = dao.insertEmployee(employee);
+//        System.out.println(insertedEmployee);
+//        dao.deleteEmployee(101);
+//
+//        Employee employee1 = dao.getEmployee(101);
+//
+//        System.out.println(employee1);
 
-        System.out.println(insertedEmployee);
 
+        // get all records
 
+        List<Employee> employees = dao.getEmployees();
+
+        for(Employee temp : employees) {
+            System.out.println(temp);
+        }
     }
 }
--- workspace/untitled/src/main/java/com/sapient/dao/EmployeeDAO.java
+++ workspace/untitled/src/main/java/com/sapient/dao/EmployeeDAO.java
@@ -3,9 +3,12 @@ package com.sapient.dao;
 import com.sapient.beans.Employee;
 import com.sapient.connection.GetConnection;
 import com.sapient.contracts.IEmployeeDAO;
+import com.sapient.exception.EmployeeNotFoundException;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class EmployeeDAO  implements IEmployeeDAO {
@@ -37,19 +40,84 @@ public class EmployeeDAO  implements IEmployeeDAO {
 
     @Override
     public Employee getEmployee(int employeeId) {
-        return null;
+
+        String sql = "select empname, empsal from employee where empid=?";
+
+        try {
+            PreparedStatement preparedStatement = GetConnection.getMysqlConnection().prepareStatement(sql);
+
+            preparedStatement.setInt(1, employeeId);
+
+            ResultSet rs =  preparedStatement.executeQuery();
+
+            if(rs.next()){
+                Employee emp = new Employee();
+                emp.setEmpId(employeeId);
+                emp.setEmpName(rs.getString("empname"));
+                emp.setEmpSal( rs.getDouble("empsal"));
+
+                return emp;
+
+            }
+
+           throw new EmployeeNotFoundException("Sorry Employee Id " + employeeId + " not found");
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 
     @Override
     public List<Employee> getEmployees() {
-        return null;
+
+        String sql ="select empid, empname, empsal from employee";
+        List<Employee> list = new ArrayList<>();
+
+        try {
+            PreparedStatement preparedStatement =
+                    GetConnection.getMysqlConnection().prepareStatement(sql);
+
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+
+                Employee emp = new Employee();
+                emp.setEmpId(resultSet.getInt("empid"));
+                emp.setEmpName(resultSet.getString("empname"));
+                emp.setEmpSal( resultSet.getDouble("empsal"));
+
+                list.add(emp);
+            }
+            return list;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     @Override
     public void deleteEmployee(int employeeId) {
+        String sql ="delete from employee where empid=?";
+        try {
+            PreparedStatement preparedStatement = GetConnection.getMysqlConnection().
+                    prepareStatement(sql);
 
+            preparedStatement.setInt(1, employeeId);
+
+            String status = preparedStatement.executeUpdate()>0 ? "Delete" :"Not Deleted";
+            System.out.println(status);
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
+    // TODO
     @Override
     public Employee updateEmployeeSalary(int employeeId, double empSal) {
         return null;
